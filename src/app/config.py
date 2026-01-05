7ec018555aec58e176a4d8becf133dad8e86609c
CrowMother <herrin8080@gmail.com>
2026-01-04 18:13:30 -0600

    Using webhook instead of discord bot

--- src/app/config.py
+++ src/app/config.py
@@ -13,7 +13,7 @@ def load_config() -> Config:
     return Config(
         schwab_app_key=os.environ["SCHWAB_APP_KEY"],
         schwab_app_secret = os.environ["SCHWAB_APP_SECRET"],
-        discord_webhook=os.environ["DISCORD_WEBHOOK"],
         discord_channel=os.environ["DISCORD_CHANNEL_ID"],
+        discord_webhook=os.environ["DISCORD_WEBHOOK"],
         db_path=os.getenv("DB_PATH", "data/app.db"),
     )
