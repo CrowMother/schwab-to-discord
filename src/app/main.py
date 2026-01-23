38be7ba9d15b0545e6e729773839444f1e5111f5
CrowMother <herrin8080@gmail.com>
2026-01-22 20:38:45 -0600

    condense main

--- src/app/main.py
+++ src/app/main.py
@@ -26,6 +26,40 @@ from .models.config import load_single_value, load_config
 from .utils.logging import setup_logging
 from .api.schwab import SchwabApi
 
+def load_trade_orders(raw_orders=None, conn=None):
+        for order in raw_orders:
+            logger.debug(f"loading trade: {order}")
+            trade = load_trade(order)
+            logger.debug(f"Loaded trade: {trade}")
+
+            # store trade
+            trade_id = store_trade(conn, trade)
+            logger.debug(f"Stored trade with ID: {trade_id}")
+            ensure_trade_state(conn, trade_id)
+        
+        conn.commit()
+
+def send_unposted_trades(conn, config, unposted_trade_ids):
+    for trade_id in unposted_trade_ids:
+        # load trade
+        trade = load_trade_from_db(conn, trade_id)
+        if not trade:
+            continue
+        
+        template = load_single_value("TEMPLATE", None)
+        #build message
+        msg = build_discord_message_template(template, trade)
+        #post to discord
+        resp = post_webhook(config.discord_webhook, msg, timeout=config.schwab_timeout)
+        logger.debug(f"Posted trade ID {trade_id} to Discord, response: {resp}")
+        #mark posted
+        with conn:
+            mark_posted(conn, trade_id, discord_message_id=None)         
+    # Figure out if I want to create a list for all these trades or process them directly after loading
+    conn.commit()
+    conn.close()
+
+
 logger = logging.getLogger(__name__)
 
 def main() -> None:
@@ -45,50 +79,12 @@ def main() -> None:
     print(f"client created: {client}")
 
     raw_orders = client.get_orders(config)
-
-    #load into dataclass
-    for order in raw_orders:
-        logger.debug(f"loading trade: {order}")
-        trade = load_trade(order)
-        logger.debug(f"Loaded trade: {trade}")
-
-        # store trade
-        trade_id = store_trade(conn, trade)
-        logger.debug(f"Stored trade with ID: {trade_id}")
-        ensure_trade_state(conn, trade_id)
+    #load into database
+    load_trade_orders(raw_orders, conn)
     
-    conn.commit()
-
-    #pull unposted trades
-
+    #pull unposted trades and send to discord
     unposted_trade_ids = get_unposted_trade_ids(conn)
-
-    for trade_id in unposted_trade_ids:
-        trade = load_trade_from_db(conn, trade_id)
-        if not trade:
-            continue
-        
-        template = load_single_value("TEMPLATE", None)
-
-        msg = build_discord_message_template(template, trade)
-        resp = post_webhook(config.discord_webhook, msg, timeout=config.schwab_timeout)
-
-        with conn:
-            mark_posted(conn, trade_id, discord_message_id=None)
-
-    
-        # normailze / format trade for discord
-        
-        # post to discord
-        
-    # Figure out if I want to create a list for all these trades or process them directly after loading
-
-    conn.commit()
-    conn.close()
-    # After figuring out data flow, implement storing to DB 
-    # Figure out DB schema or how to make it flexible to structure changes
-    # 
-    # and then posting to Discord
+    send_unposted_trades(conn, config, unposted_trade_ids)
 
 if __name__ == "__main__":
     main()
